fdb74a0effae122db4e7bc7ea35e4a6b0e351334
yweer <ywz_er@163.com>
2025-06-03 11:27:49 +0800

    Update main.py
    仅保留天翼云

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import aliyunpan, tianyiyunpan,YouDao_user_login, imaotai
 def run():
     content =''
     title = ""
-    if youdao_user != None:
-        youdao_sign=YouDao_user_login.Youdao(youdao_user, redis_info)
-        content= youdao_sign.run() + '\n\n'
-        title = "【有道】"
+    # if youdao_user != None:
+    #     youdao_sign=YouDao_user_login.Youdao(youdao_user, redis_info)
+    #     content= youdao_sign.run() + '\n\n'
+    #     title = "【有道】"
     # if ali_refresh_token != None :
     #     Aliyun=aliyunpan.Ali(ali_refresh_token, redis_info)
     #     content += Aliyun.run() + '\n\n'
@@ -16,13 +16,14 @@ def run():
     if ty_user != None and ty_pwd != None:
         content += tianyiyunpan.main(ty_user, ty_pwd)+'\n\n'
         title += "【天翼】"
-    if redis_info != None:
-        datas = RedisUtil(redis_info).get('IMAOTAI') # 获取Redis取得茅台配置数据
-        _check_item = datas.get("IMAOTAI", [])[0]
-        content+= imaotai.IMAOTAI(check_item=_check_item).main()
-        title += "【茅台】"
+    # if redis_info != None:
+    #     datas = RedisUtil(redis_info).get('IMAOTAI') # 获取Redis取得茅台配置数据
+    #     _check_item = datas.get("IMAOTAI", [])[0]
+    #     content+= imaotai.IMAOTAI(check_item=_check_item).main()
+    #     title += "【茅台】"
 
     send = MessageSend()
     send.send_all(message_tokens,title+'每日签到',content)
+
 if __name__ == "__main__":
     run()
